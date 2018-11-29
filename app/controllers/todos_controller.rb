78157dee8b161829d9f6ff083a5ef5a9651ea9d9
igacio <ignacio.olea.c@gmail.com>
2018-11-29 12:51:19 -0300

    create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,15 @@ class TodosController < ApplicationController
     def new
       @todo = Todo.new
     end
+
+    def create
+        @post = Todo.new(params[:post])
+        @post.save
+        redirect_to todos_path
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:description, :completed)
+    end
 end
